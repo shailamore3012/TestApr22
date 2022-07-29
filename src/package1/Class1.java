fcbb0133da7ceae8993878e4daa08935a7b184d9
Shaila More <shailabhoyate@gmail.com>
2022-07-29 08:47:52 +0530

    Branch1

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,8 +3,8 @@ package package1;
 public class Class1 {
 	
 	public static void main(String[]args) {
-		System.out.println("Hi");
-		System.out.println("New");
+		System.out.println("shaila");
+		System.out.println("swara");
 	}
 
 }
