d27b3bca48d7e77beab3657df5c2b37944b60880
Shaila More <shailabhoyate@gmail.com>
2022-07-29 08:54:22 +0530

    Branch2

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -5,6 +5,8 @@ public class Class1 {
 	public static void main(String[]args) {
 		System.out.println("Hi");
 		System.out.println("New");
+		System.out.println("Branch2");
+		System.out.println("Branch2_1");
 	}
 
 }
